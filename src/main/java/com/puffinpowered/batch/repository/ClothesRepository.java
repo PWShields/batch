0011a9ed0d2bdb74e2923a4d8cba3d2edcd58f2e
PWShields <paul.pw.shields@gmail.com>
2020-07-26 17:35:45 +1000

    Revert "Initial commit"
    This reverts commit afc6d70e

--- src/main/java/com/puffinpowered/batch/repository/ClothesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.puffinpowered.batch.repository;
-
-import com.puffinpowered.batch.entity.ClothingItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClothesRepository extends JpaRepository<ClothingItem, Long> {
-}
